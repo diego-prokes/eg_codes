56ab7c9e834c595a8f1559d08e2ab9237a7eba4a
Diego Prokes Herbage <diego.prokes@gmail.com>
2021-08-16 00:54:07 -0400

    Se modificó el programa que scrapea el sitio New Game. Ahora devuelve el valor de los teclados que ofrece la página ordenados por precio de forma decreciente.

--- python/Web-Scraping/scrapNewGame.py
+++ python/Web-Scraping/scrapNewGame.py
@@ -10,9 +10,12 @@ producto = productos.find_all('a')
 for info in producto:
     nombre = info.find('div', class_='juego-name').text
     precio = info.find('div', class_='juego-precio borders6').text
+    precio = float(precio.replace('$', ''))
     data.append((nombre, precio))
 
-f = open("./NewGameKeyboards.csv", "w+")
+data.sort(key=lambda x:x[1])
+
+f = open("./scrapNewGame.csv", "w+")
 f.write("Teclado,precio\n")
 for teclado in data:
     f.write("{},{}\n".format(teclado[0], teclado[1]))
